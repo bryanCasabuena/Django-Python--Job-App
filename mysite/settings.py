726564b3ac548cf8683dd967a196338a444baea7
bryanCasabuena <bryancasabuenac@gmail.com>
2024-05-18 20:08:20 +0800

    django sending email

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'carlocasabuens@gmail.com'
+EMAIL_HOST_PASSWORD = 'unjq ycbv zgou alwu'
